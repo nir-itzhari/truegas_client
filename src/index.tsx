ab2d6fb2e6b33d60f6a4e3f1a1db43725123181f
nir-itzhari <synexprox@gmail.com>
2024-02-26 18:14:41 +0200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import router from './Components/LayoutArea/Routing/Routing';
+import { PrimeReactProvider } from "primereact/api";
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <PrimeReactProvider>
+      <RouterProvider router={router} />
+    </PrimeReactProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
