c494aa87d83b1f5b34259dc470d0719ebf24c2ca
nir-itzhari <synexprox@gmail.com>
2024-02-26 22:59:15 +0200

    useMobileHook added

---
+++ src/Components/hooks/useMobileHook.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+export const useMobile = (): boolean => {
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+
+    useEffect(() => {
+        const mobileCheck = window.matchMedia('(max-width: 768px)');
+        const handleResize = (e: MediaQueryListEvent) => {
+            setIsMobile(e.matches);
+        };
+
+        // Set initial value
+        setIsMobile(mobileCheck.matches);
+
+        // Listen for changes in screen size
+        mobileCheck.addEventListener('change', handleResize);
+
+        // Cleanup
+        return () => {
+            mobileCheck.removeEventListener('change', handleResize);
+        };
+    }, []);
+
+    return isMobile;
+};
+
