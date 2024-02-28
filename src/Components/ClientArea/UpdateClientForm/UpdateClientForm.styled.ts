c8b5c3cda39cd45aa3cdbccdaf4ad5f83ae6d2a3
nir-itzhari <synexprox@gmail.com>
2024-02-28 20:01:57 +0200

    update client form added

---
+++ src/Components/ClientArea/UpdateClientForm/UpdateClientForm.styled.ts
@@ -0,0 +1,86 @@
+import { Button } from "@mui/material";
+import styled from "styled-components";
+
+export const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+export const FormContainer = styled.div`
+  direction: rtl;
+  width: 400px;
+  padding: 40px;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+`;
+
+export const FormTitle = styled.h2`
+  font-size: 24px;
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+export const Form = styled.form``;
+
+export const FormGroup = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const Label = styled.label`
+  font-size: 18px;
+  margin-bottom: 5px;
+`;
+
+export const Input = styled.input`
+  width: calc(100% - 20px); /* Adjusted width to accommodate padding */
+  padding: 10px; /* Reduced padding */
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 8px; /* Reduced border-radius */
+`;
+
+export const TextArea = styled.textarea`
+  width: calc(100% - 20px); /* Adjusted width to accommodate padding */
+  padding: 10px; /* Reduced padding */
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 8px; /* Reduced border-radius */
+  resize: vertical;
+`;
+
+export const Select = styled.select`
+  width: 100%; /* Adjusted width to accommodate padding */
+  padding: 20px; /* Reduced padding */
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 8px; /* Reduced border-radius */
+  background-color: #fff;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  &:focus {
+    outline: none;
+    border-color: #007bff;
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  }
+`;
+
+export const Option = styled.option`
+  font-size: 16px;
+`;
+
+export const submitButtonWrapper = styled.div`
+  width: 100%;
+  text-align: center;
+`
+
+export const SubmitButton = styled(Button)`
+  width: 80%;
+  font-size: 20px;
+  background-color: #007bff;
+  color: #fff;
+  border-radius: 12px;
+  cursor: pointer;
+`;
---
+++ src/Components/ClientArea/UpdateClientForm/UpdateClientForm.tsx
@@ -0,0 +1,99 @@
+import * as styled from './UpdateClientForm.styled';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { israel_cities } from '../../../Utils/data';
+import { TextField } from '@mui/material';
+import clientService from '../../../Services/ClientService';
+import Autocomplete from '@mui/material/Autocomplete';
+import { IoMdSend } from "react-icons/io";
+import { useParams } from 'react-router-dom';
+
+
+export const UpdateClientFrom = () => {
+    const params = useParams()
+    const { clientId } = params
+    const { register, handleSubmit, setValue } = useForm();
+    const [cities] = useState<string[]>(israel_cities)
+    const [newCityValue, setNewCityValue] = useState<string>(null)
+    const [currentCityValue, setCurrentCityValue] = useState<string>(null)
+
+
+    const onSubmit = async (client: any) => {
+        client.city = newCityValue
+        client.assignment_id = []
+        if (client) {
+            const addedClient = await clientService.addNewClient(client)
+            console.log(addedClient);
+        }
+    };
+
+
+    const fetchData = async () => {
+        try {
+            const clientToUpdate = await clientService.getOneClient(clientId)
+            if (clientToUpdate) {
+                Object.entries(clientToUpdate).map(([key, value]) => (
+                    setValue(`${key}`, value)
+                ))
+                setCurrentCityValue(clientToUpdate.city)
+            }
+        } catch (error: any) {
+            console.log(error.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [clientId])
+
+
+    const defaultProps = {
+        options: cities,
+        getOptionLabel: (option: string) => option
+    };
+
+
+    return (
+        <styled.FormWrapper>
+            <styled.FormContainer>
+                <styled.FormTitle>עדכון פרטי לקוח</styled.FormTitle>
+                <styled.Form onSubmit={handleSubmit(onSubmit)}>
+                    <styled.FormGroup>
+                        <TextField dir={'rtl'} fullWidth={true} id="standard-basic" label="שם מלא" variant="standard" {...register('fullName')} autoComplete='off' required />
+                    </styled.FormGroup>
+                    <styled.FormGroup>
+                        <Autocomplete {...defaultProps}
+                            dir='rtl'
+                            blurOnSelect
+                            isOptionEqualToValue={(option, value) => option === value}
+                            onChange={(e, newValue) => { setNewCityValue(newValue) }}
+                            disablePortal
+                            value={!newCityValue && currentCityValue}
+                            renderInput={(params) => (
+                                <TextField {...params} label="בחר עיר" autoComplete='on' variant="standard" />
+                            )}
+                        />
+                    </styled.FormGroup>
+                    <styled.FormGroup>
+                        <TextField dir={'rtl'} fullWidth={true} id="standard-basic" label="רחוב" variant="standard" {...register('street')} autoComplete='off' required />
+                    </styled.FormGroup>
+                    <styled.FormGroup>
+                        <TextField dir={'rtl'} fullWidth={true} id="standard-basic" label="מספר בית" variant="standard" {...register('buildingNumber')} autoComplete='off' required />
+                    </styled.FormGroup>
+                    <styled.FormGroup>
+                        <TextField dir={'rtl'} fullWidth={true} id="standard-basic" label="מספר דירה" variant="standard" {...register('apartmentNumber')} autoComplete='off' required />
+                    </styled.FormGroup>
+                    <styled.FormGroup>
+                        <TextField dir={'rtl'} fullWidth={true} id="standard-basic" label="קומה" variant="standard" {...register('floor')} autoComplete='off' required />
+                    </styled.FormGroup>
+                    <styled.FormGroup>
+                        <TextField dir={'rtl'} fullWidth={true} id="standard-basic" label="נייד" variant="standard" {...register('phoneNumber')} autoComplete='off' required />
+                    </styled.FormGroup>
+                    <styled.submitButtonWrapper>
+                        <styled.SubmitButton dir='ltr' type="submit" variant="contained" endIcon={<IoMdSend />}>הוסף</styled.SubmitButton>
+                    </styled.submitButtonWrapper>
+                </styled.Form>
+            </styled.FormContainer>
+        </styled.FormWrapper>
+    );
+};
