588967d39a71e2fd1f14a96f091a4c76a0a0894a
nir-itzhari <synexprox@gmail.com>
2024-03-05 19:02:18 +0200

    add sending id with the req

--- src/Services/SearchInputServices.ts
+++ src/Services/SearchInputServices.ts
@@ -15,11 +15,12 @@ class SearchInputService {
     public async getClientsBySearch(parameters: any): Promise<ClientModel[]> {
         // let clients: ClientModel[] = store.getState().clientsState.clients
         // if (clients.length < 0) {
-            const fullName = parameters.fullName || '0'
-            const city = parameters.city || '0'
-            const street = parameters.street || '0'
-            const forthParam = parameters.param || '0'
-            const response = await axios.get<ClientModel[]>(config.clientsSearchUrl + `${fullName}` + `/${city}` + `/${street}`);
+        const userId = store.getState().authState.user._id
+        const fullName = parameters.fullName || '0'
+        const city = parameters.city || '0'
+        const street = parameters.street || '0'
+        const forthParam = parameters.param || '0'
+        const response = await axios.get<ClientModel[]>(config.clientsSearchUrl + `${userId}` + `/${fullName}` + `/${city}` + `/${street}`);
         // }
         const clients = response.data;
         return clients;
