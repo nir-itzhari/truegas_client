af764894f368263d6e97edea8b8a1d40f26df610
nir-itzhari <synexprox@gmail.com>
2024-03-12 21:53:40 +0200

    fix search input service

--- src/Services/SearchInputServices.ts
+++ src/Services/SearchInputServices.ts
@@ -1,7 +1,7 @@
 import config from "../Utils/Config";
 import store from "../Redux/Store";
 import { ClientModel } from "../Models/ClientModel";
-import { deleteClientAction, fetchClientsAction } from "../Redux/ClientState";
+import { fetchClientsAction } from "../Redux/ClientState";
 import axios from "axios";
 import { updateInputFullName } from "../Redux/SearchInputState";
 
@@ -13,26 +13,19 @@ class SearchInputService {
     }
 
     public async getClientsBySearch(parameters: any): Promise<ClientModel[]> {
-        // let clients: ClientModel[] = store.getState().clientsState.clients
-        // if (clients.length < 0) {
-        const userId = store.getState().authState.user._id
-        const fullName = parameters.fullName || '0'
-        const city = parameters.city || '0'
-        const street = parameters.street || '0'
-        const forthParam = parameters.param || '0'
-        const response = await axios.get<ClientModel[]>(config.clientsSearchUrl + `${userId}` + `/${fullName}` + `/${city}` + `/${street}`);
-        // }
-        const clients = response.data;
-        return clients;
+
+        if (store.getState().clientsState.clients.length === 0) {
+            const userId = store.getState().authState.user._id
+            const fullName = parameters.fullName || '0'
+            const city = parameters.city || '0'
+            const street = parameters.street || '0'
+            const forthParam = parameters.param || '0'
+            const response = await axios.get<ClientModel[]>(config.clientsSearchUrl + `${userId}` + `/${fullName}` + `/${city}` + `/${street}`);
+            const clients = response.data;
+            store.dispatch(fetchClientsAction(clients))
+        }
+        return store.getState().clientsState.clients;
     }
-    public updateSearchInputCity(city: string): void {
-
-    }
-
-    public updateSearchInputStreet(street: string): void {
-
-    }
-
 }
 const searchInputService = new SearchInputService();
 
