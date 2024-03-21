56f1b41eb9086c048a69279037eed6f6b80439d3
nir-itzhari <synexprox@gmail.com>
2024-03-21 22:57:16 +0200

    connect the pagination with redux

--- src/Components/AssignmetnsArea/AssignmentList/AssignmentTable/Pagination.tsx
+++ src/Components/AssignmetnsArea/AssignmentList/AssignmentTable/Pagination.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Paginator, PaginatorPageChangeEvent } from 'primereact/paginator';
+import store from "../../../../Redux/Store";
+import { updateInputFirst, updateInputRows } from "../../../../Redux/SearchInputState";
 
 interface Props {
     total: number;
-    first: number;
-    rows: number;
-    onPageChange: (event: PaginatorPageChangeEvent) => void;
 }
 
+const TablePagination: React.FC<Props> = ({ total }: Props) => {
+    const [first, setFirst] = useState<number>(store.getState().searchInputState.first);
+    const [rows, setRows] = useState<number>(store.getState().searchInputState.rows);
 
+    useEffect(() => {
+        const unsubscribe = store.subscribe(() => {
+            const { first, rows } = store.getState().searchInputState;
+            setFirst(first);
+            setRows(rows);
+        });
 
+        return () => unsubscribe();
+    }, []);
 
-const TablePagination: React.FC<Props> = ({ total, first, rows, onPageChange }: Props) => {
     const onPageChangeInternal = (event: PaginatorPageChangeEvent) => {
-        onPageChange(event);
+        store.dispatch(updateInputFirst(event.first));
+        store.dispatch(updateInputRows(event.rows));
     };
 
     return (
