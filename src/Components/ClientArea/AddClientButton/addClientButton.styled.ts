b8ff7be62da82644d93139bc95783a52c73fc01d
nir-itzhari <synexprox@gmail.com>
2024-03-24 01:04:11 +0200

    styles

--- src/Components/ClientArea/AddClientButton/addClientButton.styled.ts
+++ src/Components/ClientArea/AddClientButton/addClientButton.styled.ts
@@ -3,12 +3,13 @@ import styled from "styled-components";
 
 export const addButtonWrapper = styled.div`
     display: inline-flex;
-    padding: 10px;
+    padding: 15px;
     flex-direction: row;
     gap: 10px;
     margin: 10px;
+    color: white;
     align-items: center;
-    background-color: #8888888c;
+    background-color: #ef8e3c;
     border-radius: 15px;
     cursor: pointer;
 `
