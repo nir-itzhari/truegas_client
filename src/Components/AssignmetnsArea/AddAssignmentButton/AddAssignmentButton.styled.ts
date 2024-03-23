697fe1bea83310831951ba2db03c460aef93a98f
nir-itzhari <synexprox@gmail.com>
2024-03-24 01:02:51 +0200

    some style

--- src/Components/AssignmetnsArea/AddAssignmentButton/AddAssignmentButton.styled.ts
+++ src/Components/AssignmetnsArea/AddAssignmentButton/AddAssignmentButton.styled.ts
@@ -2,16 +2,19 @@ import styled from "styled-components";
 
 
 export const addButtonWrapper = styled.div`
-    display: inline-flex;
-    padding: 10px;
+    display: flex;
+    padding: 15px;
     flex-direction: row;
     gap: 10px;
+    width: calc(100% - 16px);
     margin: 10px;
     color: white;
     align-items: center;
     background-color: #ef8e3c;
-    border-radius: 15px;
+    border-radius: 12px;
     cursor: pointer;
+    justify-content: center;
+    letter-spacing: 3px;
 `
 
 
