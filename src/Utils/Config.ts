0bebf7b3b9af7aba09759c2d9a70eed1bbc63023
nir-itzhari <synexprox@gmail.com>
2024-03-25 01:16:26 +0200

    ip changed

--- src/Utils/Config.ts
+++ src/Utils/Config.ts
@@ -3,18 +3,18 @@ class Config {
 }
 class DevelopmentConfig extends Config {
     public isDevelopment = true;
-    public usersUrl = "http://localhost:3001/api/users/";
-    public assignmentsUrl = "http://localhost:3001/api/assignments/";
-    public assignmentsChartUrl = "http://localhost:3001/api/assignments/chart/";
-    public assignmentsImagesUrl = "http://localhost:3001/api/images/";
-    public assignmentsAmountCardUrl = "http://localhost:3001/api/assignments/count-card/";
+    public usersUrl = "http://54.226.176.32:3001/api/users/";
+    public assignmentsUrl = "http://54.226.176.32:3001/api/assignments/";
+    public assignmentsChartUrl = "http://54.226.176.32:3001/api/assignments/chart/";
+    public assignmentsImagesUrl = "http://54.226.176.32:3001/api/images/";
+    public assignmentsAmountCardUrl = "http://54.226.176.32:3001/api/assignments/count-card/";
 
-    public baseUrl = "http://localhost:3001/api/";
-    public clientsUrl = "http://localhost:3001/api/clients/";
-    public clientsSearchUrl = "http://localhost:3001/api/clients/search/";
-    public registerUrl = "http://localhost:3001/api/auth/signup";
-    public loginUrl = "http://localhost:3001/api/auth/signin";
-    public resetPasswordUrl = "http://localhost:3001/api/auth/reset-password/";
+    public baseUrl = "http://54.226.176.32:3001/api/";
+    public clientsUrl = "http://54.226.176.32:3001/api/clients/";
+    public clientsSearchUrl = "http://54.226.176.32:3001/api/clients/search/";
+    public registerUrl = "http://54.226.176.32:3001/api/auth/signup";
+    public loginUrl = "http://54.226.176.32:3001/api/auth/signin";
+    public resetPasswordUrl = "http://54.226.176.32:3001/api/auth/reset-password/";
     // public socketUrl = "http://localhost:3000";
 }
 
